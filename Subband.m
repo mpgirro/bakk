b29a998498cd52c5d62e76ee95a1b60a20fec746
Maximilian Irro <max@irro.at>
2014-01-11 15:39:53 +0100

    Refactored the variable names in the Subband class

--- Subband.m
+++ Subband.m
@@ -2,11 +2,10 @@ classdef Subband < handle
     %UNTITLED2 Summary of this class goes here
     %   Detailed explanation goes here
     
-    properties
-        E=0;    % Energy Level 
-        c=[];   % coefficients
-        d=[];   % modified coefficients    
-        i=[];   % Absolute Positions of the Coefficients in the Wavelet Coef Band
+    properties(SetAccess = public)  
+        energy=0;    % Energy Level 
+        cArray=[];   % coefficients
+        positionArray=[];   % Absolute Positions of the Coefficients in the Wavelet Coef Band
     end
         
 end

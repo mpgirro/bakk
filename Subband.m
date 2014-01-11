fcef0a6735ca037ea0855a9916b7dbd7cd4b9834
Maximilian Irro <max@irro.at>
2014-01-11 01:27:23 +0100

    Klasse für Subbänder hinzugefügt

---
+++ Subband.m
@@ -0,0 +1,13 @@
+classdef Subband < handle
+    %UNTITLED2 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        E=0;    % Energy Level 
+        c=[];   % coefficients
+        d=[];   % modified coefficients    
+        i=[];   % Absolute Positions of the Coefficients in the Wavelet Coef Band
+    end
+        
+end
+

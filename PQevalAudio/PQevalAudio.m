4730fcbac8c1fc11f8ec4fd81e9277ea882b9948
Maximilian Irro <max@irro.at>
2014-01-12 01:09:09 +0100

    Now returns the ODG

--- PQevalAudio/PQevalAudio.m
+++ PQevalAudio/PQevalAudio.m
@@ -1,5 +1,5 @@
 % function PQevalAudio (refSignal, testSignal)
-function PQevalAudio (Fref, Ftest, StartS, EndS)
+function [ODG] = PQevalAudio (Fref, Ftest, StartS, EndS)
 % Perceptual evaluation of audio quality.
 
 % - StartS shifts the frames, so that the first frame starts at that sample.

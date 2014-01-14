6368f3ff0beb2c91861e604664d92fb62fc5300d
Maximilian Irro <max@irro.at>
2014-01-14 12:49:02 +0100

    Die neue encoder(…) Funktion sollte ich vll auch mal tranken
    Former-commit-id: 167c0e4ce55c626bcc7498d2ee3bf53bc38bd29e

---
+++ encoder.m
@@ -0,0 +1,58 @@
+
+addpath('PQevalAudio');
+addpath('PQevalAudio/CB');
+addpath('PQevalAudio/MOV');
+addpath('PQevalAudio/Misc');
+addpath('PQevalAudio/Patt');
+
+
+path = ['resources',filesep,'audio',filesep,'flute.wav'];
+[signal,fs] = wavread(path);
+
+% resample if need be - PQevalAudio only works with 48kHz
+if fs ~= 48000
+    signal = resample(signal, 48000, fs);
+    fs = 48000;
+end
+
+origSignal = signal;
+
+% aplayer = audioplayer(signal,fs);
+% aplayer.play();
+
+%plot( [1:size(signal)], signal)
+
+payload = [1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0];
+
+segmentLength = (3*8 * 64); % (3L * 2^k * (Lw+Ls), segment length to encode 1 bit (Lw+Ls=1, dwt level k=6, subband length L=8)
+segmentCount = floor(size(signal)/segmentLength);
+
+windowStart=1;
+windowEnd = segmentLength;
+for i=1:segmentCount
+    
+    signalSegment = signal(windowStart:windowEnd);
+    
+    modSignalSegment = insertbit(signalSegment,payload(i));
+    
+    signal(windowStart:windowEnd) = modSignalSegment;
+    
+    windowStart = windowStart + segmentLength;
+    windowEnd = windowEnd + segmentLength;
+end
+
+modSignal = signal;
+
+audiowrite('watermarked_audio.wav',modSignal, 48000);
+
+    
+%plot( [1:size(signal)], signal)
+
+% aplayer = audioplayer(signal,fs);
+% aplayer.play();
+
+
+% wavwrite(signal,fs,'test.wav')
+
+
+

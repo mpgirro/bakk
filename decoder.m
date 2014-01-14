3c7e87c3d230165f725c2b0500b5bef23dbbe434
Maximilian Irro <max@irro.at>
2014-01-14 12:48:25 +0100

    Decoder geschrieben, liefert falsche Payload. Ist aber so einfach, dass der kaum falsch sein kann. Muss also falsch inserted sein
    Former-commit-id: e0133b70c656ace78778b4ef7f10cebd0bd6d710

---
+++ decoder.m
@@ -0,0 +1,36 @@
+
+addpath('PQevalAudio/CB');
+addpath('PQevalAudio/MOV');
+addpath('PQevalAudio/Misc');
+addpath('PQevalAudio/Patt');
+
+
+path = 'watermarked_audio.wav';
+[signal,fs] = wavread(path);
+
+segmentLength = (3*8 * 64); % (3L * 2^k * (Lw+Ls), segment length to encode 1 bit (Lw+Ls=1, dwt level k=6, subband length L=8)
+segmentCount = floor(size(signal)/segmentLength);
+
+payloadBuffer = [];
+payloadLength = 0;
+
+windowStart=1;
+windowEnd = segmentLength;
+for i=1:segmentCount
+    
+    signalSegment = signal(windowStart:windowEnd);
+    
+    bit = extractbit(signalSegment);
+    
+    % add the bit to the recovered payload 
+    payloadLength = payloadLength + 1;
+    payloadBuffer(payloadLength) = bit;
+    
+    windowStart = windowStart + segmentLength;
+    windowEnd = windowEnd + segmentLength;
+end
+
+payloadBuffer
+
+% TODO: delete the sync sequence from the payload
+
--- extractbit.m
+++ extractbit.m
@@ -1,7 +1,49 @@
-function [ bit, success ] = extractbit( signalSegment )
+function [ bit ] = extractbit( signalSegment )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
+DWT_WAVELET = 'db2';
+DWT_LEVELS = 6;
+SUBBAND_LENGTH = 8;
+SUBBAND_COUNT = 3;
+
+[decompositionVector,bookkeepingVector] = wavedec(signalSegment, DWT_LEVELS, DWT_WAVELET);
+
+% create unique class instances, therefore don't use repmat(Subband(),3,1)
+for i=1:SUBBAND_COUNT
+    S(i) = Subband();
+end
+
+S(1).posArray = [1 : SUBBAND_LENGTH];
+S(2).posArray = [SUBBAND_LENGTH+1 : 2*SUBBAND_LENGTH];
+S(3).posArray = [2*SUBBAND_LENGTH : 3*SUBBAND_LENGTH];
+
+for i=1:SUBBAND_COUNT
+    % copy corresponding coefficients
+    S(i).coefArray = decompositionVector(S(i).posArray);
+    
+    % we only sum up the absolute values, so apply abs() to every element
+    S(i).coefArray = arrayfun(@abs,S(i).coefArray);
+    
+    % calculate the energy level
+    S(i).energy = sum(S(i).coefArray);
+end
+
+[energyMap, strMap] = drawmaps(S);
+
+Emin = strMap('min').energy;
+Emed = strMap('med').energy;
+Emax = strMap('max').energy;
+
+% calculate energy difference 
+A = Emax - Emed; 
+B = Emed - Emin;
+
+if A > B
+    bit = 1;
+else
+    bit = 0;
+end
 
 end
 

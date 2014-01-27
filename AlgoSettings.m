a84d46ad6b7c62452400c9e2117e6db1476217d5
Maximilian Irro <max@irro.at>
2014-01-27 16:47:21 +0100

    Wieder mal ein neuer Name für die Algorithmensettings
    Former-commit-id: b831e1c84c7623d22150393d37e739a7690bbd18

---
+++ AlgoSettings.m
@@ -0,0 +1,10 @@
+classdef AlgoSettings
+    properties (Constant = true)
+        DWT_WAVELET = 'db1';
+        DWT_LEVELS = 6;
+        SUBBAND_COUNT = 3;
+        SUBBAND_LENGTH = 8;
+        EMBEDDING_STRENGTH_FACTOR = 10;
+    end
+end
+

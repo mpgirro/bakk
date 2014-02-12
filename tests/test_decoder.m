6e39d7f39ecb04bbff81a5c90323a27882cf02bf
Maximilian Irro <max@irro.at>
2014-02-12 14:59:48 +0100

    Pretty output bei den Tests
    Former-commit-id: f0a5ac935ceda9aee121fb9a1f4d7c904abe9422

--- tests/test_decoder.m
+++ tests/test_decoder.m
@@ -5,4 +5,9 @@ path   = ['..',filesep,'results',filesep,'watermarked_audio.wav'];
 
 [signal,fs] = audioread(path);
 
-decoder(signal)
+watermark = decoder(signal);
+watermark = watermark';
+bitCount = size(watermark);
+fprintf('Decoded %d watermark bits\n',bitCount(1));
+fmt=['Watermark data: ' repmat('%d ',1,bitCount(1)) '\n'];
+fprintf(fmt,watermark);
--- tests/test_encoder.m
+++ tests/test_encoder.m
@@ -1,4 +1,6 @@
 
+addpath('../');
+
 wmkData = [1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0];
 
 inputPath   = ['..',filesep,'resources',filesep,'audio',filesep,'flute.wav'];
@@ -8,4 +10,9 @@ outputPath  = ['..',filesep,'results',filesep,'watermarked_audio.wav'];
 
 [modSignal, encodedBitCount] = encoder(signal, wmkData);
 
+dataStructLen = Setting.sync_sequence_length + Setting.wmk_sequence_length; % in bit
+encodedStructCount = floor(encodedBitCount/dataStructLen);
+fprintf('Encoded %d bit total in %d data struct packages holding %d watermark bit\n',encodedBitCount,encodedStructCount, encodedStructCount * Setting.wmk_sequence_length);
+
 audiowrite(outputPath,modSignal, fs);
+fprintf('Result written to %s\n',path);

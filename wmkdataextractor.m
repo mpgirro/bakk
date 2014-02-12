8ab039860e0d36dea5974c600a5c9e2c32bcfa56
Maximilian Irro <max@irro.at>
2014-02-12 13:45:08 +0100

    better start tracking the new wmkdataextractor function!
    Former-commit-id: 1e93d6fc66da15b4a7bf05f0d5c7d431e36c61cf

---
+++ wmkdataextractor.m
@@ -0,0 +1,22 @@
+function [ wmkData ] = wmkdataextractor( sample_sequence )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+windowWidth = Setting.coefficient_segment_length;
+wmkSequenceLen = Setting.wmk_sequence_length;
+wmkData = zeros([1,wmkSequenceLen]); % preallocate space
+
+sampleCursor = 1;
+
+for i=1:wmkSequenceLen
+    
+    window = sampleCursor : sampleCursor+windowWidth-1;
+    wmkData(i) = extractbit(sample_sequence(window));
+    
+    sampleCursor = sampleCursor+windowWidth;
+  
+end
+
+
+end
+

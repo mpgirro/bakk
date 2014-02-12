b9672494042872a0fb56cbc0964f801b1e112426
Maximilian Irro <max@irro.at>
2014-02-12 13:50:16 +0100

    question: what happens if the watermark is not inserted starting with sample 1? right! it still works! BÄM
    Former-commit-id: bfabbdf348ea358468cc33cfcb3e9214ee09cb00

---
+++ tests/test_shiftedencodingposition.m
@@ -0,0 +1,11 @@
+wmkData = [1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0];
+
+inputPath   = ['..',filesep,'resources',filesep,'audio',filesep,'flute.wav'];
+outputPath  = ['..',filesep,'results',filesep,'watermarked_audio.wav'];
+
+[signal,fs] = audioread(path);
+
+signalSize = size(signal);
+[modSignal, encodedBitCount] = encoder(signal(7529:signalSize(1)), wmkData);
+
+audiowrite(outputPath,modSignal, fs);

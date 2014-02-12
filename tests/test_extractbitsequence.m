9dc8732209828c8c733248ec063d3946ecddf53c
Maximilian Irro <max@irro.at>
2014-02-12 13:49:42 +0100

    ne wrest that simply decoded the bits in the audio file, starting from sample one, sync codes are ignored
    Former-commit-id: 47efd59804359284dcc29028306adb5826f29fc3

---
+++ tests/test_extractbitsequence.m
@@ -0,0 +1,30 @@
+% just read the encoded bit sequence of a signal
+
+
+path   = ['..',filesep,'results',filesep,'watermarked_audio.wav'];
+
+[signal,fs] = audioread(path);
+
+segmentLength = Setting.coefficient_segment_length; % (3L * 2^k * (Lw+Ls), segment length to encode 1 bit (Lw+Ls=1, dwt level k=6, subband length L=8)
+segmentCount = floor(size(signal)/segmentLength);
+
+payloadBuffer = [];
+payloadLength = 0;
+
+windowStart=1;
+windowEnd = segmentLength;
+for i=1:segmentCount
+
+    signalSegment = signal(windowStart:windowEnd);
+
+    bit = extractbit(signalSegment);
+
+    % add the bit to the recovered payload
+    payloadLength = payloadLength + 1;
+    payloadBuffer(payloadLength) = bit;
+
+    windowStart = windowStart + segmentLength;
+    windowEnd = windowEnd + segmentLength;
+end
+
+payloadBuffer

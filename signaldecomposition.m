eaba06162ff888785da01cd36c739de2b20eb05d
Maximilian Irro <max@irro.at>
2014-02-13 09:40:05 +0100

    Refactoring: functionality of drawmaps moved into signaldecomposition, no multiple usage of this functionality any more
    Former-commit-id: d017dfc27ba0b4243b3d28f88615c169303c9036

--- signaldecomposition.m
+++ signaldecomposition.m
@@ -1,48 +1,64 @@
 function [ decomposition ] = signaldecomposition( signalSegment )
 
-    [decompositionVector,bookkeepingVector] = wavedec(signalSegment, Setting.dwt_level, Setting.dwt_wavelet);
+[decompositionVector,bookkeepingVector] = wavedec(signalSegment, Setting.dwt_level, Setting.dwt_wavelet);
 
-    % create unique class instances, therefore don't use repmat(Subband(),3,1)
-    for i=1:Setting.SUBBAND_COUNT
-        S(i) = Subband();
-    end
-    
-    S(1).posArray = [ 1 : Setting.subband_length ];
-    S(2).posArray = [ Setting.subband_length+1 : 2*Setting.subband_length ];
-    S(3).posArray = [ 2*Setting.subband_length+1 : 3*Setting.subband_length ];
-    
-    for i=1:Setting.SUBBAND_COUNT
-        % copy corresponding coefficients
-        S(i).coefArray = decompositionVector(S(i).posArray(:));
-        
-        % we only sum up the absolute values, so apply abs() to every element
-        %S(i).coefArray = arrayfun(@abs,S(i).coefArray);
-        
-        % calculate the energy level
-        S(i).energy = sum(abs(S(i).coefArray(:)));
-    end
-    
-    [~, subbandMap] = drawmaps(S);
-    
-    Emin = subbandMap('min').energy;
-    Emed = subbandMap('med').energy;
-    Emax = subbandMap('max').energy;
-    
-    % calculate energy difference
-    A = Emax - Emed;
-    B = Emed - Emin;
+% create unique class instances, therefore don't use repmat(Subband(),3,1)
+for i=1:Setting.SUBBAND_COUNT
+    subbands(i) = Subband();
+end
 
-	% TODO: wir werden alle werte in eine structur spucken
-	decomposition = struct( 'Signal', signalSegment, ...
-                            'DecompositionVector', decompositionVector, ...
-                            'BookkeepingVector', bookkeepingVector, ...
-                            'Subbands', S, ...
-                            'Emin', Emin, ...
-                            'Emed', Emed, ...
-                            'Emax', Emax, ...
-                            'A', A, ...
-                            'B', B, ...
-                            'SubbandMap', subbandMap);
+bandWidth = Setting.subband_length;
+
+subbands(1).posArray = [ 1 : bandWidth ];
+subbands(2).posArray = [ bandWidth+1 : 2*bandWidth ];
+subbands(3).posArray = [ 2*bandWidth+1 : 3*bandWidth ];
+
+for i=1:Setting.SUBBAND_COUNT
+    % copy corresponding coefficients
+    subbands(i).coefArray = decompositionVector(subbands(i).posArray(:));
+    
+    % calculate the energy level
+    subbands(i).energy = sum(abs(subbands(i).coefArray(:)));
+end
+
+
+%   emap...Energy -> classpointer
+%   smap...String -> classpointer
+energyMap = containers.Map('KeyType','double','ValueType','any');
+subbandMap = containers.Map('KeyType','char','ValueType','any');
+
+
+for i=1:3
+    energyMap(subbands(i).energy) = subbands(i);
+end
+
+unsorted_E = [subbands(:).energy];
+sorted_E = sort(unsorted_E);
+
+subbandMap('min') = energyMap(sorted_E(1));
+subbandMap('med') = energyMap(sorted_E(2));
+subbandMap('max') = energyMap(sorted_E(3));
+
+
+Emin = subbandMap('min').energy;
+Emed = subbandMap('med').energy;
+Emax = subbandMap('max').energy;
+
+% calculate energy difference
+A = Emax - Emed;
+B = Emed - Emin;
+
+% TODO: wir werden alle werte in eine structur spucken
+decomposition = struct( 'Signal', signalSegment, ...
+    'DecompositionVector', decompositionVector, ...
+    'BookkeepingVector', bookkeepingVector, ...
+    'Subbands', subbands, ...
+    'Emin', Emin, ...
+    'Emed', Emed, ...
+    'Emax', Emax, ...
+    'A', A, ...
+    'B', B, ...
+    'SubbandMap', subbandMap);
 
 end
 

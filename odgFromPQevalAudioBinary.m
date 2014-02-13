d8cc68fa5622d075985a9528502a9b57eb308e00
Maximilian Irro <max@irro.at>
2014-02-13 12:01:05 +0100

    fixed this ODG calculation Methode
    Former-commit-id: 635c63c12fdec7a48104c7137a2b021527c4008b

--- odgFromPQevalAudioBinary.m
+++ odgFromPQevalAudioBinary.m
@@ -1,27 +1,32 @@
 function [odg] = odgFromPQevalAudioBinary( refSignal, refFs, testSignal, testFs)
 
-	% resample if need be - PQevalAudio only works with 48kHz
-	if refFs ~= 48000
-	    signal = resample(refSignal, 48000, refFs);
-	    refFs = 48000;
-	end
+% resample if need be - PQevalAudio only works with 48kHz
+if refFs ~= 48000
+    refSignal = resample(refSignal, 48000, refFs);
+    refFs = 48000;
+end
 
-	if testFs ~= 48000
-	    signal = resample(testSignal, 48000, testFs);
-	    testFs = 48000;
-	end
+if testFs ~= 48000
+    testSignal = resample(testSignal, 48000, testFs);
+    testFs = 48000;
+end
 
-	% write signals to harddrive
-	audiowrite('tmp/refSignal.wav',  refSignal,  48000);
-	audiowrite('tmp/testSignal.wav', testSignal, 48000);
+% write signals to harddrive
+audiowrite('../tmp/refSignal.wav',  refSignal,  refFs);
+audiowrite('../tmp/testSignal.wav', testSignal, testFs);
 
-	% http://www.mathworks.de/de/help/matlab/ref/system.html
-	[status,cmdout] = system('bin/PQevalAudio tmp/refSignal.wav tmp/testSignal.wav');
-	outputLength = size(cmdout);
-	odgLine = 'Objective Difference Grade: ';
-	odgLineLength = size(odgLine);
-	odgLinePos = findstr(cmdout,odgLine);
-	odgVal = cmdout(odgLinePos+odgLineLength(2):outputLength(2));
-	odg = str2double(odgVal);
+% http://www.mathworks.de/de/help/matlab/ref/system.html
+[status,cmdout] = system('../lib/bin/PQevalAudio ../tmp/refSignal.wav ../tmp/testSignal.wav');
+
+if status == 0
+    outputLength = size(cmdout);
+    odgLine = 'Objective Difference Grade: ';
+    odgLineLength = size(odgLine);
+    odgLinePos = findstr(cmdout,odgLine);
+    odgVal = cmdout(odgLinePos+odgLineLength(2):outputLength(2));
+    odg = str2double(odgVal);
+else
+    odg = NaN;
+end
 
 end

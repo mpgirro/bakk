25819564928e8ba2d7da7740eb57f3382aa17623
Maximilian Irro <max@irro.at>
2014-02-13 12:01:25 +0100

    first time this ODG calculation method works in MATLAB!
    Former-commit-id: 1e3c40d2031ae1c760655583c276a4a77a4bd259

--- odgFromEaqualExe.m
+++ odgFromEaqualExe.m
@@ -1,27 +1,30 @@
 function [odg] = odgFromEaqualExe( refSignal, refFs, testSignal, testFs)
 
-	% resample if need be - PQevalAudio only works with 48kHz
-%	if refFs ~= 48000
-%	    signal = resample(refSignal, 48000, refFs);
-%	    refFs = 48000;
-%	end
-%
-%	if testFs ~= 48000
-%	    signal = resample(testSignal, 48000, testFs);
-%	    testFs = 48000;
-%	end
+% write signals to harddrive
+audiowrite('../tmp/refSignal.wav',  refSignal,  refFs);
+audiowrite('../tmp/testSignal.wav', testSignal, testFs);
 
-	% write signals to harddrive
-	audiowrite('tmp/refSignal.wav',  refSignal,  refFs);
-	audiowrite('tmp/testSignal.wav', testSignal, testFs);
 
-	% http://www.mathworks.de/de/help/matlab/ref/system.html
-	[status,cmdout] = system('/Applications/Wine.app/Contents/Resources/bin/wine bin/EAQUAL.exe tmp/refSignal.wav tmp/testSignal.wav');
-	outputLength = size(cmdout);
-	odgLine = 'Objective Difference Grade: ';
-	odgLineLength = size(odgLine);
-	odgLinePos = findstr(cmdout,odgLine);
-	odgVal = cmdout(odgLinePos+odgLineLength(2):outputLength(2));
-	odg = str2double(odgVal);
+command = '/Applications/Wine.app/Contents/Resources/bin/wine ../lib/bin/EAQUAL.exe -fref ../tmp/refSignal.wav -ftest ../tmp/testSignal.wav -silent ../tmp/eaqual-output.txt';
+
+% http://www.mathworks.de/de/help/matlab/ref/system.html
+[status,~] = system(command);
+
+% http://www.mathworks.de/de/help/matlab/ref/unix.html
+%[status,cmdout] = unix(command);
+
+if status == 0 || status == 68 % system() return this, who knows why...
+    
+    % EAQUAL executed in silent mode - not output (matlab does not work
+    % with executables which update their statues - it fails with an error
+    % code). Instead the output is written into a txt file in ../tmp/
+    output = textread('../tmp/eaqual-out.txt', '%s', 'delimiter', '\n', 'whitespace', '');
+    
+    cellValues = textscan(output{3},'%f');
+    matValues = cell2mat(cellValues);
+    odg = matValues(1);
+else
+    odg = NaN;
+end
 
 end

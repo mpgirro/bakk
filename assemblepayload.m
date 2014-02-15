78adc3a164f77d0c614f78ac2f007174bbd01f5e
Maximilian Irro <max@irro.at>
2014-02-15 17:44:30 +0100

    window now calculated with a cursor too, uses the new Setting attributes
    Former-commit-id: 1155c625a32a8fed8cf1d773ac3a6c1ea594af7d

--- assemblepayload.m
+++ assemblepayload.m
@@ -1,59 +1,67 @@
 function [ payload ] = assemblepayload( watermark )
 %UNTITLED Assambles the payload that is to be encoded into a signal
 %   A Watermark can not just be encoded by itself. Due to TSM and for WMK
-%   detection there have to be synchronization code segments, that are 
-%   hopefully distinctivly detectable without any false positive error. 
+%   detection there have to be synchronization code segments, that are
+%   hopefully distinctivly detectable without any false positive error.
 %   The payload consists of two parts. The synchronisation code and the
 %   watermark data. There is always a whole synchronization code followed
 %   by X bit of watermark data. Then there is a synchronization code
-%   segment again. If the watermark data does not satisfy 
+%   segment again. If the watermark data does not satisfy
 %   (data mod wmk_sequence_length = 0), then the watermark has to be
-%   extended with dummy bits. 
+%   extended with dummy bits.
 
 syncCode = Setting.sync_code;
-syncSequenceLength  = Setting.synccode_block_sequence_length;
+syncBlockSequenceLength  = Setting.synccode_block_sequence_length;
 wmkBlockSequenceLength   = Setting.wmkdata_block_sequence_length;
+dataStructBlockSequenceLength = Setting.datastruct_package_sequence_length;
 
 wmkSize = size(watermark);
-
+wmkSize = wmkSize(2);
 
 
 % check if watermark needs dummy bits
 % extend watermark if need be
-if mod(wmkSize(2), wmkBlockSequenceLength) ~= 0
-   missing = wmkBlockSequenceLength - mod(wmkSize(2), wmkBlockSequenceLength);
-   watermark(wmkSize(2)+1: wmkSize(2)+missing) = 0;
-   wmkSize = size(watermark);
+if mod(wmkSize, wmkBlockSequenceLength) ~= 0
+    missing = wmkBlockSequenceLength - mod(wmkSize, wmkBlockSequenceLength);
+    watermark(wmkSize+1: wmkSize+missing) = 0;
+    wmkSize = size(watermark);
 end
 
 
-wmkSegmentCount = ceil(wmkSize(2)/wmkBlockSequenceLength);
+wmkBlockCount = ceil(wmkSize/wmkBlockSequenceLength);
 
 
 % reshape the watermark, so we can index the segments easily
-watermark = reshape(watermark, wmkBlockSequenceLength, wmkSegmentCount);
+watermark = reshape(watermark, wmkBlockSequenceLength, wmkBlockCount);
 watermark = watermark';
 
 
 % for each wmk_segment there is a sync_code
 % therefore total payload length is as follows:
-payloadLength = syncSequenceLength * wmkSegmentCount + wmkBlockSequenceLength * wmkSegmentCount;
+payloadLength = dataStructBlockSequenceLength * wmkBlockCount;
 
-payload = zeros([1, payloadLength]); % preallocate 1 x bitCount array 
+payload = zeros([1, payloadLength]); % preallocate 1 x bitCount array
 
-windowStart = 1;
-windowEnd = syncSequenceLength;
-for i=1:wmkSegmentCount
+
+payloadCursor = 1;
+for i=1:wmkBlockCount
+    
+    window = payloadCursor : payloadCursor+syncBlockSequenceLength-1;
     
     % insert sync code
-    payload(windowStart:windowEnd) = syncCode;
-    windowStart = windowStart   + syncSequenceLength;
-    windowEnd   = windowEnd     + syncSequenceLength;
+    payload(window) = syncCode;
     
-    % insert watermark segment
-    payload(windowStart:windowEnd) = watermark(i,:);
-    windowStart = windowStart   + wmkBlockSequenceLength;
-    windowEnd   = windowEnd     + wmkBlockSequenceLength;
+    % move cursor to the next wmk data block
+    payloadCursor = payloadCursor+syncBlockSequenceLength;
+
+    
+    window = payloadCursor : payloadCursor+wmkBlockSequenceLength-1;
+    
+    % insert watermark block
+    payload(window) = watermark(i,:);
+    
+    % move cursor to the next sync block
+    payloadCursor = payloadCursor + wmkBlockSequenceLength;
     
 end
 

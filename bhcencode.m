effe2cb6891846877ad7d9fc1f719d8407b817f4
Maximilian Irro <max@irro.at>
2014-03-20 22:43:49 +0100

    it is BCH, one and for all
    Former-commit-id: 34f3c48a5a76620b8f63c06e8a8083e6f073585d

--- bhcencode.m
+++
@@ -1,9 +0,0 @@
-function [ bhc_data ] = bhcencode( pure_daza )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-hEnc = comm.BCHEncoder;
-bhc_data = step(hEnc, pure_daza);
-
-end
-

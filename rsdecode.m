1c93d69755dc0563fa1a047c3c6bfd66f82291b2
Maximilian Irro <max@irro.at>
2014-05-08 18:09:22 +0200

    reed-solomon encoder and decoder
    Former-commit-id: dcb3552b17b19f505c026fcbc946f3e89efc86cc

--- rsdecode.m
+++ rsdecode.m
@@ -2,7 +2,11 @@ function [ pure_data ] = rsdecode( rs_data )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
-hDec = comm.RSDecoder;
+
+messageLength  = Setting.message_length;
+codewordLength = Setting.codeword_length;
+
+hDec = comm.RSDecoder('MessageLength', messageLength, 'CodewordLength', codewordLength, 'BitInput', true);
 pure_data = step(hDec, rs_data');
 
 end
--- rsencode.m
+++ rsencode.m
@@ -2,7 +2,10 @@ function [ rs_data ] = rsencode( pure_data )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-hEnc = comm.RSEncoder;
+messageLength  = Setting.message_length;
+codewordLength = Setting.codeword_length;
+
+hEnc = comm.RSEncoder('MessageLength', messageLength, 'CodewordLength', codewordLength, 'BitInput', true);
 rs_data  = step(hEnc, pure_data');
 
 end

bb0c5bf796c4f6dcc5b3bb35f21ca77e0219b9f6
Maximilian Irro <max@irro.at>
2014-05-08 20:10:45 +0200

    advanced default test functions - now writes binary numbers and decodes them for better error tracking
    Former-commit-id: 5594166bd405ef339f8b66831cd8c98c89496ba7

--- tests/test_decoder.m
+++ tests/test_decoder.m
@@ -10,6 +10,14 @@ path = ['..',filesep,'results',filesep,'watermarked_audio.wav'];
 
 watermark = decoder(signal);
 
+messageLength = Setting.message_length;
+wmk_bin = reshape(watermark, messageLength, numel(watermark)/messageLength)';
+wmk_dec = bi2de(wmk_bin,'left-msb');
+
+disp('    Dec              Binary')
+disp('   -----   -------------------------')
+disp([wmk_dec,wmk_bin])
+
 % watermark = watermark';
 % bitCount = size(watermark);
 % fprintf('Decoded %d watermark bits\n',bitCount(1));
--- tests/test_encoder.m
+++ tests/test_encoder.m
@@ -3,20 +3,25 @@ clear;
 
 addpath('../');
 
-wmkData = repmat([1,1,1,1,0,0,0,0],1,1000);
-%wmkData = [1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0];
+%wmkData = repmat([1,1,1,0,0],1,1000);
 
-%inputPath   = ['..',filesep,'resources',filesep,'audio',filesep,'der-affe-ist-gut.wav'];
-inputPath   = ['..',filesep,'resources',filesep,'audio',filesep,'flute.wav'];
+% create a matrix as wide as the message blocks 
+% and filled with the binary numbers from 0 to 2^messageBlock-1
+messageLength = Setting.message_length;
+max_binnum = 2 ^ messageLength - 1;
+bin_numbers = de2bi(0:max_binnum,'left-msb');
+wmkData = reshape(bin_numbers',1,numel(bin_numbers));
+
+inputPath   = ['..',filesep,'resources',filesep,'audio',filesep,'der-affe-ist-gut.wav'];
+%inputPath   = ['..',filesep,'resources',filesep,'audio',filesep,'flute.wav'];
 outputPath  = ['..',filesep,'results',filesep,'watermarked_audio.wav'];
 
 [signal,fs] = audioread(inputPath);
-
 [modSignal, encodedBitCount] = encoder(signal, wmkData, fs);
 
 dataStructLen = Setting.synccode_block_sequence_length + Setting.wmkdata_block_sequence_length; % in bit
-encodedStructCount = floor(encodedBitCount/dataStructLen);
-%fprintf('Encoded %d bit total in %d data struct packages holding %d watermark bit\n',encodedBitCount,encodedStructCount, encodedStructCount * Setting.wmk_sequence_length);
+% encodedStructCount = floor(encodedBitCount/dataStructLen);
+% fprintf('Encoded %d bit total in %d data struct packages holding %d watermark bit\n',encodedBitCount,encodedStructCount, encodedStructCount * Setting.wmk_sequence_length);
 
 audiowrite(outputPath,modSignal, fs);
 fprintf('Result written to %s\n',outputPath);

f8d4afdfd5121e391283b44786cae43e9bf30d9b
Maximilian Irro <max@irro.at>
2014-06-06 19:06:39 +0200

    moved binaries and scripts to bin/
    Former-commit-id: 7972f72b91fa49a311fe08b100da71f6a4ff2375

---
+++ bin/stirmark_test.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 3 ]
+then
+	echo "usage: ./stirmark_test <testfile> <samplerate> <channels>"
+	exit 1
+fi
+
+# read command line args
+testfile=$1
+samplerate=$2
+channels=$3
+
+# this will be 
+streamfile="teststream"
+
+# run these attack with these params on the file
+attack_list=(	"AddNoise:100"
+				"AddDynNoise:20"
+        		"ZeroCross:1000"
+        		"ZeroLength:10"
+        		"Amplify:50" 
+				"RC_LowPass:9000"
+				"FFT_Invert:16384"
+				"FFT_RealReverse:16384"
+			)
+			
+# first convert the audio file to a stream
+./read_write_stream -f $testfile -c 1 > $streamfile
+ 
+# run all attacks and convert results to audiofiles
+for attack in "${attack_list[@]}" ; do
+    attack_name=${attack%%:*}
+    attack_param=${attack#*:}
+    ./smfa2 --$attack_name $attack_param -s $samplerate < $streamfile | ./read_write_stream -p -s $samplerate -c 1 -f ${testfile%.wav}-attacked-$attack_name-$attack_param.wav
+done
+
+
